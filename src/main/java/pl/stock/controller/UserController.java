89b9c2a460a26afa3640a77f59cdc57e0f94df66
EwaBujara <ewa.bujara@gmail.com>
2019-03-08 13:57:59 +0100

    User Controller

--- src/main/java/pl/stock/controller/UserController.java
+++ src/main/java/pl/stock/controller/UserController.java
@@ -1,4 +1,43 @@
 package pl.stock.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.stock.entity.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "user/registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute("userForm") User userForm,
+                               BindingResult bindingResult,
+                               HttpServletRequest request,
+                               HttpSession session) {
+
+//        newUserValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "user/registration";
+        }
+
+//        userService.save(userForm);
+        session.setAttribute("currentUser", userForm);
+
+        return "redirect:"+request.getContextPath()+"/group/1";
+    }
 }
