bf1de602acf04ba8d7bc0512f7a8c6affbff222e
EwaBujara <ewa.bujara@gmail.com>
2019-03-09 06:38:20 +0100

    WalletService-buyStock function

--- src/main/java/pl/stock/service/WalletService.java
+++ src/main/java/pl/stock/service/WalletService.java
@@ -1,4 +1,8 @@
 package pl.stock.service;
 
+import pl.stock.entity.Stock;
+import pl.stock.entity.User;
+
 public interface WalletService {
+    void buyStock(User user, Stock stock, int quantity);
 }
--- src/main/java/pl/stock/service/WalletServiceImpl.java
+++ src/main/java/pl/stock/service/WalletServiceImpl.java
@@ -1,7 +1,21 @@
 package pl.stock.service;
 
 import org.springframework.stereotype.Service;
+import pl.stock.entity.Stock;
+import pl.stock.entity.User;
+import pl.stock.entity.Wallet;
+import pl.stock.entity.WalletItem;
 
 @Service
-public class WalletServiceImpl {
+public class WalletServiceImpl implements WalletService{
+
+    @Override
+    public void buyStock(User user, Stock stock, int quantity) {
+        WalletItem walletItem = new WalletItem();
+        walletItem.setStock(stock);
+        walletItem.setQuantity(quantity);
+
+        Wallet wallet = user.getWallet();
+
+    }
 }
