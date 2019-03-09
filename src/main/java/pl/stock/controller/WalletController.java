f3f4ad72d23bd761ffda8b4b947df6b34a37ea53
EwaBujara <ewa.bujara@gmail.com>
2019-03-09 17:59:13 +0100

    stock page

--- src/main/java/pl/stock/controller/WalletController.java
+++ src/main/java/pl/stock/controller/WalletController.java
@@ -1,12 +1,19 @@
 package pl.stock.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.stock.repository.StockRepository;
 
 @Controller
 @RequestMapping("/stock")
 public class WalletController {
+
+    @Autowired
+    StockRepository stockRepository;
 
     @GetMapping("/wallet")
     public String showWallet(){
@@ -17,5 +24,11 @@ public class WalletController {
     @GetMapping("/account")
     public String showAccount(){
         return "wallet/account";
+    }
+
+    @GetMapping("/stock/{stockId}")
+    public String showDetails(@PathVariable Long stockId, Model model){
+        model.addAttribute("stock", stockRepository.findOne(stockId));
+        return "wallet/stock";
     }
 }
--- src/main/webapp/WEB-INF/views/wallet/stock.jsp
+++ src/main/webapp/WEB-INF/views/wallet/stock.jsp
@@ -6,19 +6,23 @@
     <title>Stock Details</title>
 </head>
 <body>
-<div class="p-3 mb-2 bg-dark text-white text-center container col-6">
-    <table class="table table-striped">
+<div class="p-3 mb-2 bg-dark text-white text-center container col-8">
+    <table class="table text-white">
         <tr>
             <th>Name</th>
             <th>Code</th>
             <th>Unit</th>
             <th>Price</th>
+            <th>Total Quantity</th>
+            <th>Available Quantity</th>
         </tr>
         <tr>
             <td>${stock.name}</td>
             <td>${stock.code}</td>
             <td>${stock.unit}</td>
             <td>${stock.price}</td>
+            <td>${stock.totalQuantity}</td>
+            <td>${stock.availableQuantity}</td>
         </tr>
     </table>
     <a class="btn btn-info" href="http://localhost:8080/stock/withdraw">Withdraw</a>
